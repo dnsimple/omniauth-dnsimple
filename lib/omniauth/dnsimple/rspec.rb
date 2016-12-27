cb67ee6dd111e97b3286609f67511eff982242c7
Luca Guidi <me@lucaguidi.com>
2016-12-27 15:09:08 +0100

    Add RSpec testing support

---
+++ lib/omniauth/dnsimple/rspec.rb
@@ -0,0 +1,65 @@
+require "securerandom"
+require "omniauth"
+
+module OmniAuth
+  module Dnsimple
+    module RSpec
+      def self.included(spec) # rubocop:disable Metrics/MethodLength
+        spec.class_eval do
+          let(:successful_params) do
+            Hash[
+              type:            "account",
+              plan_identifier: "dnsimple-professional",
+              uid:             Random.rand(10_000_000),
+              email:           "user@#{SecureRandom.uuid}.test",
+              token:           SecureRandom.hex(32),
+            ]
+          end
+        end
+      end
+
+      private
+
+      PROVIDER = :dnsimple
+
+      # rubocop:disable Metrics/AbcSize
+      # rubocop:disable Metrics/MethodLength
+      def omniauth_successful_signup
+        params = Hash[
+          "omniauth.auth" => {
+            "provider" => PROVIDER.to_s,
+            "uid"      => successful_params.fetch(:uid),
+            "info"     => {
+              "type"            => successful_params.fetch(:type),
+              "plan_identifier" => successful_params.fetch(:plan_identifier),
+              "email"           => successful_params.fetch(:email)
+            },
+            "credentials" => {
+              "token" => successful_params.fetch(:token)
+            }
+          }
+        ]
+
+        ::OmniAuth::AuthHash.new(params.fetch("omniauth.auth")).tap do |data|
+          omniauth_mock_auth data
+        end
+      end
+      # rubocop:enable Metrics/MethodLength
+      # rubocop:enable Metrics/AbcSize
+
+      def omniauth_failing_signup
+        omniauth_mock_auth :invalid_credentials
+      end
+
+      def omniauth_mock_auth(data)
+        ::OmniAuth.config.mock_auth[PROVIDER] = data
+      end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include(OmniAuth::Dnsimple::RSpec)
+end
+
+OmniAuth.config.test_mode = true
